5db8c8fb8e4f244afea52ed640b9d3ed9644b469
dengchenyang.brady <dengchenyang.brady@bytedance.com>
2021-07-04 18:36:43 +0800

    [dengchenyang] L24

---
+++ src/ListNode.java
@@ -0,0 +1,23 @@
+/**
+ * @author dengchenyang.brady@bytedance.com
+ * @Description
+ * @Date 2021/7/4
+ **/
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
+
