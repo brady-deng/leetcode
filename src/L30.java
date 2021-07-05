0c8c826ebf22df9c2547558596799b4df7307d5e
dengchenyang.brady <dengchenyang.brady@bytedance.com>
2021-07-05 21:50:56 +0800

    [dengchenyang] 0705

---
+++ src/L30.java
@@ -0,0 +1,88 @@
+import java.util.*;
+
+/**
+ * @author dengchenyang.brady@bytedance.com
+ * @description
+ * @date 2021/7/5
+ **/
+public class L30 {
+    public static void main(String[] args) {
+        System.out.println("Please input the String:");
+        Scanner scanner = new Scanner(System.in);
+        String tmp = scanner.nextLine();
+        System.out.println("Please input the num of words:");
+        int n = scanner.nextInt();
+        String[] words = new String[n];
+        scanner.nextLine();
+        for (int i = 0;i<n;i++) {
+            System.out.println("Please input the word");
+            words[i] = scanner.nextLine();
+        }
+
+        System.out.println(findSubstring(tmp, words));
+
+    }
+    public static List<Integer> findSubstring(String s, String[] words) {
+        int m = words.length;
+        int n = words[0].length();
+        List<Integer> res = new ArrayList<>();
+        Map<String, Integer> m1 = buildWordMap(words);
+        int i = 0;
+        StringBuilder prev = new StringBuilder("");
+        while (i < s.length()-m*n+1){
+            if (!(!res.isEmpty() && prev.toString().equals(s.substring(res.get(res.size()-1)+m*n,i+m*n)) && prev.length() == n)) {
+                prev.append(s.charAt(i));
+                String[] tmp = splitWords(s.substring(i, i+m*n), m, n);
+                Map<String, Integer> m2 = buildWordMap(tmp);
+                if (comparaWordMap(m1, m2)) {
+                    res.add(i);
+                    prev = new StringBuilder(s.substring(i,i+1));
+                }
+                i+=1;
+            }
+            else {
+                res.add(i);
+                i+=n;
+                prev = new StringBuilder("");
+            }
+        }
+        return res;
+    }
+
+    public static String[] splitWords(String tmp, int m, int n) {
+        String[] res = new String[m];
+        for (int i = 0;i<m;i++) {
+            res[i] = tmp.substring(i*n,(i+1)*n);
+        }
+        return res;
+    }
+
+    public static Map<String, Integer> buildWordMap(String[] words) {
+        Map<String, Integer> res = new HashMap<>();
+        for (String word:words) {
+            res.put(word, res.getOrDefault(word, 0)+1);
+        }
+        return res;
+    }
+
+    public static boolean comparaWordMap(Map<String, Integer> m1, Map<String, Integer> m2) {
+        for (Map.Entry<String, Integer> entry:m1.entrySet()) {
+            if (!m2.getOrDefault(entry.getKey(), 0).equals(entry.getValue())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static boolean comparaWords(String[] w1, String[] w2) {
+        Arrays.sort(w1);
+        Arrays.sort(w2);
+        for (int i = 0; i<w1.length;i++) {
+            if (!w1[i].equals(w2[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/L34.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * @author dengchenyang.brady@bytedance.com
+ * @date 2021/7/5
+ **/
+public class L34 {
+    public static void main(String[] args) {
+        System.out.println("Please input the nums:");
+        Scanner scanner = new Scanner(System.in);
+        int[] nums = Arrays.stream(scanner.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
+        System.out.println("Please input the target:");
+        int target = scanner.nextInt();
+        System.out.println(Arrays.toString(searchRange(nums, target)));
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        if (nums.length > 0) {
+            int m = binarySearch(nums, target, 0, nums.length-1);
+            if (m == -1) {
+                return new int[]{-1,-1};
+            }
+            else {
+                int l = m,h = m;
+                while (l >= 0 && nums[l] == target) {
+                    l--;
+                }
+                while (h >= 0 && h < nums.length && nums[h] == target) {
+                    h++;
+                }
+                return new int[]{l+1,h-1};
+            }
+        }
+        else {
+            return new int[]{-1,-1};
+        }
+    }
+
+    public static int binarySearch(int[] nums, int target, int left, int right) {
+        if (right-left>0) {
+            int m = left+(right-left)/2;
+            if (nums[m] > target) {
+                return binarySearch(nums, target, left, m-1);
+            }
+            else if (nums[m] < target) {
+                return binarySearch(nums, target, m+1, right);
+            }
+            else {
+                return m;
+            }
+        }
+        else {
+            if (nums[left] == target) {
+                return left;
+            } else {
+                return -1;
+            }
+        }
+    }
+
+
+}
