d1aa90be7f8116035db1d01218d84d72ee6fe814
brady-deng <3013506862@qq.com>
2021-07-10 23:38:29 +0800

    [dengchenyang.brady] 0710

--- src/L56.java
+++ src/L56.java
@@ -1,53 +1,53 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
-/**
- * @author dengchenyang.brady@bytedance.com
- * @date 2021/7/7
- **/
-public class L56 {
-    public static void main(String[] args) {
-        System.out.println("Please input the m:");
-        Scanner scanner = new Scanner(System.in);
-        int m = scanner.nextInt();
-        System.out.println("Please input the n:");
-        int n = scanner.nextInt();
-
-    }
-
-    public int[][] merge(int[][] intervals) {
-        for (int i = 0; i<intervals.length; i++) {
-            if (intervals[i] != null && intervals[i].length>0) {
-                for (int j = i; j<intervals.length; j++) {
-                    if (intervals[j] != null && intervals[j].length>0) {
-                        if (intervals[j][0] <= intervals[i][1] || intervals[j][1] <= intervals[i][0]) {
-                            intervals[i] = merge2Int(intervals[i], intervals[j]);
-                            intervals[j] = null;
-                        }
-                    }
-                }
-            }
-        }
-        long c = Arrays.stream(intervals).filter(item->item!=null).count();
-        int[][] res = new int[(int) c][2];
-        for ()
-    }
-
-    public int[] merge2Int(int[] i1, int[] i2) {
-        int[] res = new int[2][;
-        if (i1[1] <= i2[0]) {
-            res[0] = i1[0];
-            res[1] = i2[1];
-        }
-        else if (i1[0]>=i2[1]) {
-            res[0] = i2[0];
-            res[1] = i1[1];
-        }
-        return res;
-    }
-//    public int[][] sortInter(int[][] tmp) {
-//        int m = tmp.length;
-//        int n = tmp[0].length;
-//        int[][] res = new int[m][n];
+//import java.util.Arrays;
+//import java.util.Scanner;
+//
+///**
+// * @author dengchenyang.brady@bytedance.com
+// * @date 2021/7/7
+// **/
+//public class L56 {
+//    public static void main(String[] args) {
+//        System.out.println("Please input the m:");
+//        Scanner scanner = new Scanner(System.in);
+//        int m = scanner.nextInt();
+//        System.out.println("Please input the n:");
+//        int n = scanner.nextInt();
+//
 //    }
-}
+//
+//    public int[][] merge(int[][] intervals) {
+//        for (int i = 0; i<intervals.length; i++) {
+//            if (intervals[i] != null && intervals[i].length>0) {
+//                for (int j = i; j<intervals.length; j++) {
+//                    if (intervals[j] != null && intervals[j].length>0) {
+//                        if (intervals[j][0] <= intervals[i][1] || intervals[j][1] <= intervals[i][0]) {
+//                            intervals[i] = merge2Int(intervals[i], intervals[j]);
+//                            intervals[j] = null;
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        long c = Arrays.stream(intervals).filter(item->item!=null).count();
+//        int[][] res = new int[(int) c][2];
+//        for ()
+//    }
+//
+//    public int[] merge2Int(int[] i1, int[] i2) {
+//        int[] res = new int[2][;
+//        if (i1[1] <= i2[0]) {
+//            res[0] = i1[0];
+//            res[1] = i2[1];
+//        }
+//        else if (i1[0]>=i2[1]) {
+//            res[0] = i2[0];
+//            res[1] = i1[1];
+//        }
+//        return res;
+//    }
+////    public int[][] sortInter(int[][] tmp) {
+////        int m = tmp.length;
+////        int n = tmp[0].length;
+////        int[][] res = new int[m][n];
+////    }
+//}
---
+++ src/L71.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class L71 {
+    public static void main(String[] args) {
+        System.out.println("Please input the path:");
+        Scanner scanner = new Scanner(System.in);
+        String path = scanner.nextLine();
+        System.out.println(simplifyPath(path));
+    }
+    public static String simplifyPath(String path) {
+        Stack<String> res = new Stack<>();
+        res.push("");
+        StringBuilder tmp = new StringBuilder();
+        for (char item:path.toCharArray()) {
+            if (item == '/') {
+                String string = tmp.toString();
+                tmp = new StringBuilder();
+                if (string.equals("..") ) {
+                    if (res.size() > 1) {
+                        res.pop();
+                    }
+                }
+                else if (string.equals(".") || "".equals(string)) {
+                    continue;
+                }
+                else {
+//                res.add("/");
+                    res.push(string);
+                }
+            }
+            else {
+                tmp.append(item);
+            }
+        }
+        if (!tmp.toString().equals("/")) {
+            String string = tmp.toString();
+            if (string.equals("..") ) {
+                if (res.size() > 1) {
+                    res.remove(res.size()-1);
+                }
+            }
+            else if (string.equals(".") || "".equals(string)) {
+
+            }
+            else {
+//                res.add("/");
+                res.push(string);
+            }
+        }
+        if (res.size()>1) {
+            return String.join( "/", res);
+        }
+        else return "/";
+    }
+
+    public static List<String> dpSearch(String path, int start, List<String> res) {
+        return new ArrayList<>();
+    }
+}
