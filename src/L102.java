91d85691c9e6667234e52dc7f244a654767f2b63
brady-deng <3013506862@qq.com>
2021-07-19 23:36:13 +0800

    [dengchenyang.brady] 0719

---
+++ src/L102.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author dengchenyang.brady@bytedance.com
+ * @date 2021/7/19
+ **/
+public class L102 {
+    public static void main(String[] args) {
+        System.out.println("Please input the nodes:");
+        Scanner scanner = new Scanner(System.in);
+        String tmp = scanner.nextLine();
+        TreeNode head = TreeNode.deserialize(tmp);
+        System.out.println(levelOrder(head));
+    }
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root != null) {
+            getNodes(Collections.singletonList(root), res);
+        }
+        return res;
+    }
+
+    public static void getNodes(List<TreeNode> pre, List<List<Integer>> res) {
+        List<Integer> tmp = new ArrayList<>();
+        List<TreeNode> thisLevel = new ArrayList<>();
+        for (TreeNode item:pre) {
+            tmp.add(item.val);
+            if (item.left != null) {
+                thisLevel.add(item.left);
+            }
+            if (item.right != null) {
+                thisLevel.add(item.right);
+            }
+        }
+        res.add(tmp);
+        if (thisLevel.isEmpty()) {
+            return;
+        }
+        getNodes(thisLevel, res);
+    }
+}
--- src/L97.java
+++ src/L97.java
@@ -13,10 +13,15 @@ public class L97 {
         String s2 = scanner.nextLine();
         System.out.println("Please input the target string:");
         String tar = scanner.nextLine();
-        System.out.println(isInterLeave(s1,s2, tar));
+//        System.out.println(isInterleave(s1,s2, tar));
+        System.out.println(L97Ans.isInterleave(s1,s2, tar));
 
     }
-    public static boolean isInterLeave(String s1, String s2, String s3) {
+    public static boolean isInterleave(String s1, String s2, String s3) {
+        return myIsInterLeave(s1, s2, s3) || myIsInterLeave(s2,s1,s3);
+    }
+
+    public static boolean myIsInterLeave(String s1, String s2, String s3) {
         if ("".equals(s1) && "".equals(s2) && "".equals(s3)) {
             return true;
         }
@@ -29,20 +34,13 @@ public class L97 {
         else {
             int start = 0;
             if (s1.charAt(start) == s3.charAt(start)) {
-                while (s1.charAt(start) == s3.charAt(start)) {
+                while (start < s1.length() && start < s3.length() && s1.charAt(start) == s3.charAt(start)) {
                     start++;
-                }
-                boolean tmp = isInterLeave(s2, s1.substring(start), s3.substring(start));
-                if (tmp) {
-                    return tmp;
-                }
-                else {
-                    return isInterLeave(s2,s1,s3);
+                    boolean tmp = myIsInterLeave(s2, s1.substring(start), s3.substring(start));
+                    if (tmp) return tmp;
                 }
             }
-            else {
-                return isInterLeave(s2,s1,s3);
-            }
+            return false;
         }
     }
 }
