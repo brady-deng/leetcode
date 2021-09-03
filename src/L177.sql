ffb2d8e5a1725ffba2c4f4fa5d762edc1918906c
brady-deng <3013506862@qq.com>
2021-09-03 13:34:41 +0800

    [dengchenyang.brady] 0903

---
+++ src/L177.sql
@@ -0,0 +1,1 @@
+select distinct Salary from Employee order by Salary desc limit 1 offset N;
---
+++ src/LUtil.java
@@ -0,0 +1,26 @@
+import com.sun.xml.internal.ws.api.server.SDDocument;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author dengchenyang.brady@bytedance.com
+ * @date 2021/8/29
+ **/
+public class LUtil {
+    private static Scanner scanner = new Scanner(System.in);
+    public static int[] inputNums() {
+        System.out.println("Please input the nums:");
+        return Arrays.stream(scanner.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    public static ListNode inputNodes() {
+        System.out.println("Please input the nodes:");
+        return ListNode.deserialize(Arrays.stream(scanner.nextLine().split(",")).mapToInt(Integer::parseInt).toArray());
+    }
+
+    public static int inputNum() {
+        System.out.println("Please input the nums:");
+        return scanner.nextInt();
+    }
+}
