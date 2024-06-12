56e011b18415d8aed6c4af1eb1dc83ab51ff4944
brady-deng <3013506862@qq.com>
2024-06-12 10:52:53 +0800

    0612

---
+++ src/main/java/lc/list/L16.java
@@ -0,0 +1,73 @@
+package main.java.lc.list;
+
+//Given an integer array nums of length n and an integer target, find three
+//integers in nums such that the sum is closest to target.
+//
+// Return the sum of the three integers.
+//
+// You may assume that each input would have exactly one solution.
+//
+//
+// Example 1:
+//
+//
+//Input: nums = [-1,2,1,-4], target = 1
+//Output: 2
+//Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+//
+//
+// Example 2:
+//
+//
+//Input: nums = [0,0,0], target = 1
+//Output: 0
+//Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+//
+//
+//
+// Constraints:
+//
+//
+// 3 <= nums.length <= 500
+// -1000 <= nums[i] <= 1000
+// -10⁴ <= target <= 10⁴
+//
+//
+// Related Topics Array Two Pointers Sorting 👍 10380 👎 562
+
+
+import java.util.Arrays;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class L16 {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int[] tmp = new int[]{Integer.MAX_VALUE, target};
+        for(int i = 0; i < nums.length-2; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            recurFindRes(nums, i, i+1, nums.length-1, tmp, target);
+        }
+        return tmp[1];
+    }
+
+    public void recurFindRes(int[] nums, int i, int l, int r, int[] tmp, int target) {
+        while(l < r) {
+            int cur = nums[i] + nums[l] + nums[r];
+            int closet = Math.abs(cur-target);
+            if(closet < tmp[0]) {
+                tmp[0] = closet;
+                tmp[1] = cur;
+            }
+            if(cur == target) {
+                return;
+            }
+            else if(cur < target) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/main/java/lc/list/L18.java
@@ -0,0 +1,87 @@
+package main.java.lc.list;
+
+//Given an array nums of n integers, return an array of all the unique
+//quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+//
+//
+// 0 <= a, b, c, d < n
+// a, b, c, and d are distinct.
+// nums[a] + nums[b] + nums[c] + nums[d] == target
+//
+//
+// You may return the answer in any order.
+//
+//
+// Example 1:
+//
+//
+//Input: nums = [1,0,-1,0,-2,2], target = 0
+//Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+//
+//
+// Example 2:
+//
+//
+//Input: nums = [2,2,2,2,2], target = 8
+//Output: [[2,2,2,2]]
+//
+//
+//
+// Constraints:
+//
+//
+// 1 <= nums.length <= 200
+// -10⁹ <= nums[i] <= 10⁹
+// -10⁹ <= target <= 10⁹
+//
+//
+// Related Topics Array Two Pointers Sorting 👍 11207 👎 1372
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class L18 {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList();
+        for(int i = 0; i < nums.length-3; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            threeSum(nums, i, target, res);
+        }
+        return res;
+
+    }
+    public void threeSum(int[] nums, int start, int tar, List<List<Integer>> res) {
+        for (int i = start+1; i < nums.length-2; i++) {
+            if(i > start+1 && nums[i] == nums[i-1]) continue;
+            recurFindRes(nums, start, i, i+1, nums.length-1, res, tar);
+        }
+    }
+    public void recurFindRes(int[] nums, int start, int l, int m, int r, List<List<Integer>> res, int tar) {
+        while (m < r) {
+            long tmp = (long)nums[start]+nums[l]+nums[m]+nums[r];
+            long t = (long) tar;
+            if (tmp == t) {
+                res.add(Arrays.asList(nums[start],nums[l],nums[m],nums[r]));
+                m++;
+                r--;
+                while (m < r && nums[m] == nums[m-1]) {
+                    m++;
+                }
+                while (r > m && nums[r] == nums[r+1]) {
+                    r--;
+                }
+            }
+            else if (tmp > t) {
+                r--;
+            } else {
+                m++;
+            }
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
