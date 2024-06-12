7a79b11b9a87a0662d277375c70f8715612fcad1
brady-deng <3013506862@qq.com>
2024-06-12 09:56:35 +0800

    0611

---
+++ src/main/java/lc/node/mytest/N108Test.java
@@ -0,0 +1,64 @@
+package main.java.lc.node.mytest;
+
+import main.java.lc.common.ob.ListNode;
+import main.java.lc.common.ob.TreeNode;
+import main.java.lc.util.LUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 二叉查找树的第k大的值
+ * 8,3,10,1,6,null,14,null,null,4,7,13
+ * @author dengchenyang@tju.edu.cn
+ * @date 2024/6/10
+ **/
+public class N108Test {
+    public static void main(String[] args) {
+        TreeNode head = LUtil.inputTree();
+        int k = LUtil.inputNum();
+        System.out.println(new N108Test().getNthNode(head, k));
+        System.out.println(new N108Test().getNthNode2(head, k));
+    }
+
+    public TreeNode getNthNode(TreeNode head, int k) {
+        List<TreeNode> res = new ArrayList<>();
+        getNthNode(head, k, res);
+        return res.get(k-1);
+    }
+
+    public TreeNode getNthNode2(TreeNode head, int k) {
+        Stack<TreeNode> par = new Stack<>();
+        TreeNode cur = head;
+        int n = 0;
+        while (true) {
+            while (cur != null) {
+                par.push(cur);
+                cur = cur.right;
+            }
+            if (!par.isEmpty()) {
+                TreeNode node = par.pop();
+                n++;
+                if (n == k) return node;
+                if (node.left != null) {
+                    cur = node.left;
+                }
+            }
+        }
+    }
+
+    public void getNthNode(TreeNode head, int k, List<TreeNode> res) {
+        if (head == null) {
+            return;
+        }
+        if (res.size() >= k) {
+            return;
+        }
+        getNthNode(head.right, k, res);
+        res.add(head);
+        getNthNode(head.left, k, res);
+    }
+
+
+}
---
+++ src/main/java/lc/node/mytest/N112.java
@@ -0,0 +1,37 @@
+package main.java.lc.node.mytest;
+
+import main.java.lc.common.ob.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * 中序遍历的下一个节点
+ * @author dengchenyang@tju.edu.cn
+ * @date 2024/6/10
+ **/
+public class N112 {
+
+
+
+    public TreeNode inOrderTree(TreeNode head, TreeNode tar) {
+        Stack<TreeNode> nodes = new Stack<>();
+        TreeNode cur = head;
+        boolean flag = false;
+        while(true) {
+            while(cur != null) {
+                nodes.push(cur);
+                cur = cur.left;
+            }
+            if (!nodes.isEmpty()) {
+                TreeNode node = nodes.pop();
+                if (flag) {
+                    return node;
+                }
+                if (node == tar) {
+                    flag = true;
+                }
+                cur = node.right;
+            }
+        }
+    }
+}
